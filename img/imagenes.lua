f170f73229da2e66bab32153ec9c21710a93b77d
Rafael PacMe <rafy.pachecomendez@gmail.com>
2016-12-17 18:07:32 -0300

    Alpha 1.5: Re-Fix: Add images variables
    Signed by: @ROM-PacMe

--- img/imagenes.lua
+++ img/imagenes.lua
@@ -1,11 +1,11 @@
 -- ## SOLO DECLARACIONES DE IMAGENES ##
 
 -- ## WALLPAPER PRINCIPAL (LOCKER Y EXPLORER.EXE)
-wallpaper = image.load("wallpaper.png")
+wallpaper = image.load("img/wallpaper.png")
 
 -- ## ICONOS DE BARRA DE ESTADO ##
-wifi = image.load("wifi.png")
-battery100 = image.load("battery/100.png")
-battery50 = image.load("battery/50.png")
-battery10 = image.load("battery/10.png")
-charge = image.load("battery/charge.png")
+wifi = image.load("img/wifi.png")
+battery100 = image.load("img/battery/100.png")
+battery50 = image.load("img/battery/50.png")
+battery10 = image.load("img/battery/10.png")
+charge = image.load("img/battery/charge.png")
