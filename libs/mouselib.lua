da910a21a042a381ae9313f71b0b77f45936e1bd
Rafael PacMe <rafy.pachecomendez@gmail.com>
2016-12-20 12:54:18 -0300

    Alpha 1.6: Add Mouselib Support (Beta) Signed by: @ROM-PacMe

---
+++ libs/mouselib.lua
@@ -0,0 +1,4 @@
+--Variables para cursor mini cursor digital
+x = 240 -- centro de la pantalla
+y = 136 -- centro de la pantalla
+v = 4 -- velocidad
--- script.lua
+++ script.lua
@@ -17,6 +17,9 @@ splash.old("img/splash2.png")
 -- ## PRESENTACIÓN CON EL LOGO DE PSP ##
 splash.old("img/splash3.png")
 
+-- ## CARGA LIBRERÍA MOUSELIB ##
+dofile("libs/mouselib.lua")
+
 -- ## IR A PÁGINA DE LOGIN ##
 dofile("aviso.lua")
 screen.flip()
